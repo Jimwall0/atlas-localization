1a59c2557ff985769dd563a7d7137448b245845e
Tsimmuaj <tyang4204@gmail.com>
2025-06-05 10:08:20 -0500

    No english

--- localization/app.py
+++ localization/app.py
@@ -1,7 +1,29 @@
-#!/usr/bin/env python3
-"""
-babel test
-"""
-from babel import Babel, _
+from flask import Flask, request
+from flask_babel import Babel, _
 
-greeting = _("Hello, World!")
+app = Flask(__name__)
+
+# Configure available languages
+app.config['BABEL_DEFAULT_LOCALE'] = 'fr'
+app.config['BABEL_TRANSLATION_DIRECTORIES'] = './translations'
+
+
+babel = Babel(app)
+
+
+def get_locale():
+    # Use request headers or a user-specific preference to determine locale
+    return request.accept_languages.best_match(['en', 'fr'])
+
+
+babel = Babel(app, locale_selector=get_locale)
+
+
+@app.route('/')
+def index():
+    # This will display the string in the appropriate language
+    return _("Hello, World!")
+
+
+if __name__ == "__main__":
+    app.run()
